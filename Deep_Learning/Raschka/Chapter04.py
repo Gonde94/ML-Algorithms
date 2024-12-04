031de3faa24b41d9f8a114271e3b48b3d9b1a9a7
Nic R <nrad94@hotmail.co.uk>
2024-12-04 19:21:19 +0000

    Used black to format though made everything worse

--- Deep_Learning/Raschka/Chapter04.py
+++ Deep_Learning/Raschka/Chapter04.py
@@ -1,15 +1,15 @@
 import torch
-import torch.nn  as nn
+import torch.nn as nn
 
 GPT_CONFIG_124M = {
-    "vocab_size": 50257,    # num of words used by BPE tokenizer
-    "context_length": 1024, # max num of input tokens model can handle
-    "emb_dim": 768,         # embedding size: each token -> 768-d vector
-    "n_heads": 12,          # num of heads in multi-head attention mechanism
-    "n_layers": 12,         # num of transformer blocks in the model
-    "drop_rate": 0.1,       # 10% random dropout of hidden units 
-    "qkv_bias": False       # whether to include a bias vector in linear layers
-}                           # --> for query, key, value computations
+    "vocab_size": 50257,  # num of words used by BPE tokenizer
+    "context_length": 1024,  # max num of input tokens model can handle
+    "emb_dim": 768,  # embedding size: each token -> 768-d vector
+    "n_heads": 12,  # num of heads in multi-head attention mechanism
+    "n_layers": 12,  # num of transformer blocks in the model
+    "drop_rate": 0.1,  # 10% random dropout of hidden units
+    "qkv_bias": False,  # whether to include a bias vector in linear layers
+}  # --> for query, key, value computations
 
 
 class GELU(nn.Module):
@@ -17,10 +17,17 @@ class GELU(nn.Module):
         super().__init__()
 
     def forward(self, x):
-        return 0.5 * x * (1 + torch.tanh(
-            torch.sqrt(torch.tensor(2.0 / torch.pi)) *
-            (x + 0.44715 * torch.pow(x, 3))
-        ))
+        return (
+            0.5
+            * x
+            * (
+                1
+                + torch.tanh(
+                    torch.sqrt(torch.tensor(2.0 / torch.pi))
+                    * (x + 0.44715 * torch.pow(x, 3))
+                )
+            )
+        )
 
 
 class FeedForward(nn.Module):
@@ -29,19 +36,17 @@ class FeedForward(nn.Module):
         self.layers = nn.Sequential(
             nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
             GELU(),
-            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"])
+            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"]),
         )
 
     def forward(self, x):
         return self.layers(x)
-    
+
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, d_in, d_out, context_length,
-                 dropout, num_heads, qkv_bias=False):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
         super().__init__()
-        assert (d_out % num_heads == 0), \
-        "d_out must be divisble by num_heads"
+        assert d_out % num_heads == 0, "d_out must be divisble by num_heads"
 
         self.d_out = d_out
         self.num_heads = num_heads
@@ -50,13 +55,11 @@ class MultiHeadAttention(nn.Module):
         self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
         self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
         self.out_proj = nn.Linear(d_out, d_out)  # Linear layer to combine head outputs
-        self.dropout = nn.Dropout(dropout) 
+        self.dropout = nn.Dropout(dropout)
         self.register_buffer(
-            "mask",
-            torch.triu(torch.ones(context_length, context_length),
-            diagonal=1)
-        ) 
-        
+            "mask", torch.triu(torch.ones(context_length, context_length), diagonal=1)
+        )
+
     def forward(self, x):
         # batch, # tokens, dimension size
         b, num_tokens, d_in = x.shape
@@ -76,23 +79,25 @@ class MultiHeadAttention(nn.Module):
         queries = queries.transpose(1, 2)
         values = values.transpose(1, 2)
 
-        attn_scores = queries @ keys.transpose(2, 3) # dot product for each head
-        mask_bool = self.mask.bool()[:num_tokens, :num_tokens] # masks truncated to num_tokens
+        attn_scores = queries @ keys.transpose(2, 3)  # dot product for each head
+        mask_bool = self.mask.bool()[
+            :num_tokens, :num_tokens
+        ]  # masks truncated to num_tokens
 
-        attn_scores.masked_fill_(mask_bool, -torch.inf) # use mask to fill attention scores
+        attn_scores.masked_fill_(
+            mask_bool, -torch.inf
+        )  # use mask to fill attention scores
 
-        attn_weights = torch.softmax(
-            attn_scores / keys.shape[-1]**0.5, dim=-1
-        )
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
         attn_weights = self.dropout(attn_weights)
 
-        context_vec = (attn_weights @ values).transpose(1, 2) # (b, num_tokens, n_heads, head_dim)
+        context_vec = (attn_weights @ values).transpose(
+            1, 2
+        )  # (b, num_tokens, n_heads, head_dim)
 
         # combines heads where self.d_out = self.num_heads * self.head_dim
-        context_vec = context_vec.contiguous().view(
-            b, num_tokens, self.d_out
-        )
-        context_vec = self.out_proj(context_vec) # an optional linear projection
+        context_vec = context_vec.contiguous().view(b, num_tokens, self.d_out)
+        context_vec = self.out_proj(context_vec)  # an optional linear projection
 
         return context_vec
 
@@ -101,12 +106,12 @@ class TransformerBlock(nn.Module):
     def __init__(self, cfg):
         super().__init__()
         self.att = MultiHeadAttention(
-            d_in = cfg["emb_dim"],
-            d_out = cfg["emb_dim"],
-            context_length = cfg["context_length"],
-            num_heads = cfg["n_heads"],
-            dropout = cfg["drop_rate"],
-            qkv_bias = cfg["qkv_bias"]
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"],
+            dropout=cfg["drop_rate"],
+            qkv_bias=cfg["qkv_bias"],
         )
         self.ff = FeedForward(cfg)
         self.norm1 = LayerNorm(cfg["emb_dim"])
@@ -127,7 +132,7 @@ class TransformerBlock(nn.Module):
         x = self.drop_shortcut(x)
         x = x + shortcut
         return x
-    
+
 
 class LayerNorm(nn.Module):
     def __init__(self, emb_dim):
@@ -155,20 +160,37 @@ class GPTModel(nn.Module):
         )
 
         self.final_norm = LayerNorm(cfg["emb_dim"])
-        self.out_head = nn.Linear(
-            cfg["emb_dim"], cfg["vocab_size"], bias=False
-        )
+        self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False)
 
     def forward(self, in_idx):
         batch_size, seq_len = in_idx.shape
         tok_embeds = self.tok_emb(in_idx)
 
-        pos_embeds = self.pos_emb(
-            torch.arange(seq_len, device=in_idx.device)
-        )
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
         x = tok_embeds + pos_embeds
         x = self.drop_emb(x)
         x = self.trf_blocks(x)
         x = self.final_norm(x)
         logits = self.out_head(x)
         return logits
+
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    # idx is a (batch, n_tokens) array of indices in the current context
+    for _ in range(max_new_tokens):
+        idx_cond = idx[
+            :, -context_size:
+        ]  # crop current context if exceeds supported context size
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        logits = logits[
+            :, -1, :
+        ]  # focus only on last time step so (batch, n_token, vocab_size) -> (batch, vocab_size)
+        probas = torch.softmax(logits, dim=-1)  # shape (batch, vocab_size)
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)  # shape (batch, 1)
+        idx = torch.cat(
+            (idx, idx_next), dim=1
+        )  # append sampled index to the running sequence, idx shape (batch, n_tokens+1)
+
+    return idx
