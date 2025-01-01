67db643458f3c2d70a2c84db8e1b4308d8c8c22c
Nic R <nrad94@hotmail.co.uk>
2025-01-01 19:03:02 +0000

    Train model using LoRA

--- Deep_Learning/Raschka/src/Chapter06.py
+++ Deep_Learning/Raschka/src/Chapter06.py
@@ -1,6 +1,7 @@
 import torch
 from torch.utils.data import Dataset
 import pandas as pd
+from Chapter05 import evaluate_model
 
 class SpamDataset(Dataset):
     def __init__(self, parquet_file, tokeniser, max_length=None,
@@ -74,3 +75,60 @@ def calc_accuracy_loader(data_loader, model, device, num_batches=None):
             break
     
     return correct_predictions / num_examples
+
+
+def calc_loss_batch_class(input_batch, target_batch, model, device):
+    input_batch = input_batch.to(device)
+    target_batch = target_batch.to(device)
+    logits = model(input_batch)[:, -1, :] # logits of last output token
+    loss = torch.nn.functional.cross_entropy(
+        logits, target_batch
+    )
+    return loss
+
+
+def train_classifier_simple(model, train_loader, val_loader,
+                            optimiser, device, num_epochs,
+                            eval_freq, eval_iter):
+    
+    train_losses, val_losses, train_accs, val_accs = [], [], [], []
+    examples_seen, global_step = 0, -1
+
+    # Main training loop
+    for epoch in range(num_epochs): # step 1
+        model.train()
+
+        for input_batch, target_batch in train_loader: # step 2
+            optimiser.zero_grad() # step 3
+            loss = calc_loss_batch_class( # step 4
+                input_batch, target_batch, model, device
+            )
+            loss.backward() # step 5
+            optimiser.step() # step 6
+            examples_seen += input_batch.shape[0]
+            global_step += 1
+
+            if global_step % eval_freq == 0: # step 7
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, device, eval_iter
+                )
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                print(f"Epoch {epoch+1} (Step {global_step:06d}): "
+                      f"Train loss {train_loss:.3f}, "
+                      f"Val loss {val_loss:.3f}"
+                )
+
+        train_accuracy = calc_accuracy_loader(
+            train_loader, model, device, num_batches=eval_iter
+        )
+        val_accuracy = calc_accuracy_loader(
+            val_loader, model, device, num_batches=eval_iter
+        )
+
+        print(f"Training accuracy: {train_accuracy*100:.2f}% | ", end="")
+        print(f"Validation accuracy: {val_accuracy*100:.2f}%")
+        train_accs.append(train_accuracy)
+        val_accs.append(val_accuracy)
+
+    return train_losses, val_losses, train_accs, val_accs, examples_seen
